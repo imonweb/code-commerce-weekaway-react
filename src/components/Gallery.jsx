6ede52a7ea7337493b9eca2903cbd39811fff79a
Imon <contact@imonweb.co.uk>
2022-12-06 00:58:54 +0000

    gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Gallery = () => {
   return (
-    <div id='gallery'>
-      <h2>Gallery</h2>
-      <div>
+    <div id='gallery' className='max-w-[1140px] m-auto w-full p-4 p-16'>
+      <h2 className='text-center text-gray-700 p4'>Gallery</h2>
+      <div className='grid sm:grid-cols-5 gap-4'>
          <div className='sm:col-span-3 col-span-2 row-span-2'>
-                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1615571022219-eb45cf7faa9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="/" />
+              <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1615571022219-eb45cf7faa9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="/" />
           </div>
           <div>
               <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1594398901394-4e34939a4fd0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="/" />
