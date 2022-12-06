daf194c3f2f30e35c4f52f5ff1b9142c9b314209
Imon <contact@imonweb.co.uk>
2022-12-06 00:15:40 +0000

    nav

---
+++ src/components/Activities.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Activities = () => {
+  return (
+    <div>
+      
+      <div>
+        <h3>Resorts</h3>
+        <img 
+          src='https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' 
+          alt="" 
+          />
+      </div>
+
+      <div>
+        <h3>Resorts</h3>
+        <img 
+        src='https://images.unsplash.com/photo-1599640842225-85d111c60e6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80' 
+          alt="" 
+          />
+      </div>
+
+      <div>
+        <h3>Resorts</h3>
+        <img 
+          src='https://images.unsplash.com/photo-1544551763-46a013bb70d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+          alt="" 
+          />
+      </div>
+
+    </div>
+  )
+}
+
+export default Activities
